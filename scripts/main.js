3004ca6fbad1b16a1c3e617d5801c31b96117680
TianranZhang <tianran_zhang@brown.edu>
2022-01-04 22:57:45 -0500

    dining hall page in progress

---
+++ scripts/main.js
@@ -0,0 +1,38 @@
+var slideIndex = 1;
+var timer = null;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+    clearTimeout(timer);
+    showDivs(slideIndex += n);
+}
+
+function currentDiv(n) {
+    clearTimeout(timer);
+    showDivs(slideIndex = n);
+}
+
+function showDivs(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (!n){
+        n = ++slideIndex
+    }
+    if (slideIndex > slides.length) {
+        slideIndex = 1
+    }  
+    if (slideIndex < 1) {
+        slideIndex = slides.length
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }  
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+    timer = setTimeout(showDivs, 100000); // Change image every 2 seconds
+}
+
