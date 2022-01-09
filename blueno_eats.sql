50549b087da6004265fc58e803ef99359ab2c5f1
tlamlert <tanadol_lamlertprasertkul@brown.edu>
2022-01-09 04:40:37 -0500

    update db

--- blueno_eats.sql
+++ blueno_eats.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Jan 08, 2022 at 10:00 AM
+-- Generation Time: Jan 09, 2022 at 10:40 AM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 8.0.13
 
@@ -24,6 +24,29 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `accounts`
+--
+
+CREATE TABLE `accounts` (
+  `id` int(11) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(30) NOT NULL,
+  `username` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `accounts`
+--
+
+INSERT INTO `accounts` (`id`, `email`, `password`, `username`) VALUES
+(2, 'a', 'b', 'hello'),
+(3, 'a', 'b', 'hello'),
+(11103, 'a', 'a', 'a'),
+(61731, '', '', '');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `dish_pages`
 --
 
@@ -31,28 +54,46 @@ CREATE TABLE `dish_pages` (
   `id` int(11) NOT NULL,
   `name` varchar(200) NOT NULL,
   `content` text NOT NULL,
-  `images` text NOT NULL,
   `rating` double NOT NULL,
   `total_score` int(11) NOT NULL,
   `num_reviews` int(11) NOT NULL,
-  `one_star` int(11) NOT NULL,
-  `two_star` int(11) NOT NULL,
-  `three_star` int(11) NOT NULL,
-  `four_star` int(11) NOT NULL,
-  `five_star` int(11) NOT NULL
+  `1_star` int(11) NOT NULL,
+  `2_star` int(11) NOT NULL,
+  `3_star` int(11) NOT NULL,
+  `4_star` int(11) NOT NULL,
+  `5_star` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `dish_pages`
 --
 
-INSERT INTO `dish_pages` (`id`, `name`, `content`, `images`, `rating`, `total_score`, `num_reviews`, `one_star`, `two_star`, `three_star`, `four_star`, `five_star`) VALUES
-(1, 'Grilled Chicken', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', '<div class=\"mySlides fade\">\r\n                <img src=\"img/placeholding.png\" class=\"slide-img\">\r\n            </div>\r\n\r\n            <div class=\"mySlides fade\">\r\n                <img src=\"img/place2.jpeg\" class=\"slide-img\">\r\n            </div>\r\n\r\n            <div class=\"mySlides fade\">\r\n                <img src=\"img/place3.jpeg\" class=\"slide-img\">\r\n            </div>', 0.32, 0, 0, 0, 0, 0, 0, 0),
-(2, 'Pizza', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', '<div class=\"mySlides fade\">\r\n                <img src=\"img/placeholding.png\" class=\"slide-img\">\r\n            </div>\r\n\r\n            <div class=\"mySlides fade\">\r\n                <img src=\"img/place2.jpeg\" class=\"slide-img\">\r\n            </div>\r\n\r\n            <div class=\"mySlides fade\">\r\n                <img src=\"img/place3.jpeg\" class=\"slide-img\">\r\n            </div>', 3.21, 0, 0, 0, 0, 0, 0, 0),
-(3, 'French Fries', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', '<div class=\"mySlides fade\">\r\n                <img src=\"img/placeholding.png\" class=\"slide-img\">\r\n            </div>\r\n\r\n            <div class=\"mySlides fade\">\r\n                <img src=\"img/place2.jpeg\" class=\"slide-img\">\r\n            </div>\r\n\r\n            <div class=\"mySlides fade\">\r\n                <img src=\"img/place3.jpeg\" class=\"slide-img\">\r\n            </div>', 2.31, 0, 0, 0, 0, 0, 0, 0),
-(4, 'Salad Bar', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', '<div class=\"mySlides fade\">\r\n                <img src=\"img/placeholding.png\" class=\"slide-img\">\r\n            </div>\r\n\r\n            <div class=\"mySlides fade\">\r\n                <img src=\"img/place2.jpeg\" class=\"slide-img\">\r\n            </div>\r\n\r\n            <div class=\"mySlides fade\">\r\n                <img src=\"img/place3.jpeg\" class=\"slide-img\">\r\n            </div>', 4.76, 0, 0, 0, 0, 0, 0, 0),
-(5, 'Burger', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', '<div class=\"mySlides fade\">\r\n                <img src=\"img/placeholding.png\" class=\"slide-img\">\r\n            </div>\r\n\r\n            <div class=\"mySlides fade\">\r\n                <img src=\"img/place2.jpeg\" class=\"slide-img\">\r\n            </div>\r\n\r\n            <div class=\"mySlides fade\">\r\n                <img src=\"img/place3.jpeg\" class=\"slide-img\">\r\n            </div>', 3.56, 0, 0, 0, 0, 0, 0, 0),
-(6, 'Hot Dog', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', '<div class=\"mySlides fade\">\r\n                <img src=\"img/placeholding.png\" class=\"slide-img\">\r\n            </div>\r\n\r\n            <div class=\"mySlides fade\">\r\n                <img src=\"img/place2.jpeg\" class=\"slide-img\">\r\n            </div>\r\n\r\n            <div class=\"mySlides fade\">\r\n                <img src=\"img/place3.jpeg\" class=\"slide-img\">\r\n            </div>', 4.32, 0, 0, 0, 0, 0, 0, 0);
+INSERT INTO `dish_pages` (`id`, `name`, `content`, `rating`, `total_score`, `num_reviews`, `1_star`, `2_star`, `3_star`, `4_star`, `5_star`) VALUES
+(1, 'Grilled Chicken', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 0.32, 12, 6, 3, 1, 1, 1, 0),
+(2, 'Pizza', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 3.21, 0, 0, 0, 0, 0, 0, 0),
+(3, 'French Fries', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 2.31, 0, 0, 0, 0, 0, 0, 0),
+(4, 'Salad Bar', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 4.76, 0, 0, 0, 0, 0, 0, 0),
+(5, 'Burger', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 3.56, 0, 0, 0, 0, 0, 0, 0),
+(6, 'Hot Dog', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 4.32, 0, 0, 0, 0, 0, 0, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `dish_slideshow`
+--
+
+CREATE TABLE `dish_slideshow` (
+  `dish_id` int(11) NOT NULL,
+  `img_src` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `dish_slideshow`
+--
+
+INSERT INTO `dish_slideshow` (`dish_id`, `img_src`) VALUES
+(1, 'img/place2.jpeg'),
+(1, 'img/place3.jpeg');
 
 -- --------------------------------------------------------
 
@@ -63,20 +104,19 @@ INSERT INTO `dish_pages` (`id`, `name`, `content`, `images`, `rating`, `total_sc
 CREATE TABLE `hall_pages` (
   `id` int(11) NOT NULL,
   `official_name` varchar(100) NOT NULL,
-  `name` varchar(100) NOT NULL,
-  `slideshow` text NOT NULL
+  `name` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `hall_pages`
 --
 
-INSERT INTO `hall_pages` (`id`, `official_name`, `name`, `slideshow`) VALUES
-(1, 'Sharpe Refectory', '\"The Ratty\"', '<div class=\"mySlides fade\">\r\n  <img src=\"img/placeholding.png\" class=\"slide-img\">\r\n  <div class=\"text\">The changes of the images may be automatic and at regular intervals or they may be manually controlled by a presenter or the viewer.\r\n    </div>\r\n</div>\r\n\r\n<div class=\"mySlides fade\">\r\n  <img src=\"img/place2.jpeg\" class=\"slide-img\">\r\n  <div class=\"text\">Today in this blog I\'ll share the image slideshow effect using javascript. In this program, there are five images, and these images automatically change after a certain period. I used JavaScript to change the image after a certain time interval.\r\n  </div>\r\n</div>\r\n\r\n<div class=\"mySlides fade\">\r\n  <img src=\"img/place3.jpeg\" class=\"slide-img\">\r\n  <div class=\"text\">You can copy the codes from the given boxes or download the code files from the given link but I recommend you to download the source code files instead of copying codes because you won\'t get images if you copy the codes\r\n  </div>\r\n</div>'),
-(2, 'Andrews Commons', '\"Andrews Commons\"', '<div class=\"mySlides fade\">\r\n  <img src=\"img/placeholding.png\" class=\"slide-img\">\r\n  <div class=\"text\">The changes of the images may be automatic and at regular intervals or they may be manually controlled by a presenter or the viewer.\r\n    </div>\r\n</div>\r\n\r\n<div class=\"mySlides fade\">\r\n  <img src=\"img/place2.jpeg\" class=\"slide-img\">\r\n  <div class=\"text\">Today in this blog I\'ll share the image slideshow effect using javascript. In this program, there are five images, and these images automatically change after a certain period. I used JavaScript to change the image after a certain time interval.\r\n  </div>\r\n</div>\r\n\r\n<div class=\"mySlides fade\">\r\n  <img src=\"img/place3.jpeg\" class=\"slide-img\">\r\n  <div class=\"text\">You can copy the codes from the given boxes or download the code files from the given link but I recommend you to download the source code files instead of copying codes because you won\'t get images if you copy the codes\r\n  </div>\r\n</div>'),
-(3, 'Verney-Woolley', '\"Verney-Woolley\"', '<div class=\"mySlides fade\">\r\n  <img src=\"img/placeholding.png\" class=\"slide-img\">\r\n  <div class=\"text\">The changes of the images may be automatic and at regular intervals or they may be manually controlled by a presenter or the viewer.\r\n    </div>\r\n</div>\r\n\r\n<div class=\"mySlides fade\">\r\n  <img src=\"img/place2.jpeg\" class=\"slide-img\">\r\n  <div class=\"text\">Today in this blog I\'ll share the image slideshow effect using javascript. In this program, there are five images, and these images automatically change after a certain period. I used JavaScript to change the image after a certain time interval.\r\n  </div>\r\n</div>\r\n\r\n<div class=\"mySlides fade\">\r\n  <img src=\"img/place3.jpeg\" class=\"slide-img\">\r\n  <div class=\"text\">You can copy the codes from the given boxes or download the code files from the given link but I recommend you to download the source code files instead of copying codes because you won\'t get images if you copy the codes\r\n  </div>\r\n</div>'),
-(4, 'Ivy Room', '\"Ivy Room\"', '<div class=\"mySlides fade\">\r\n  <img src=\"img/placeholding.png\" class=\"slide-img\">\r\n  <div class=\"text\">The changes of the images may be automatic and at regular intervals or they may be manually controlled by a presenter or the viewer.\r\n    </div>\r\n</div>\r\n\r\n<div class=\"mySlides fade\">\r\n  <img src=\"img/place2.jpeg\" class=\"slide-img\">\r\n  <div class=\"text\">Today in this blog I\'ll share the image slideshow effect using javascript. In this program, there are five images, and these images automatically change after a certain period. I used JavaScript to change the image after a certain time interval.\r\n  </div>\r\n</div>\r\n\r\n<div class=\"mySlides fade\">\r\n  <img src=\"img/place3.jpeg\" class=\"slide-img\">\r\n  <div class=\"text\">You can copy the codes from the given boxes or download the code files from the given link but I recommend you to download the source code files instead of copying codes because you won\'t get images if you copy the codes\r\n  </div>\r\n</div>'),
-(5, 'Josiah\'s', '\"Josiah\'s\"', '<div class=\"mySlides fade\">\r\n  <img src=\"img/placeholding.png\" class=\"slide-img\">\r\n  <div class=\"text\">The changes of the images may be automatic and at regular intervals or they may be manually controlled by a presenter or the viewer.\r\n    </div>\r\n</div>\r\n\r\n<div class=\"mySlides fade\">\r\n  <img src=\"img/place2.jpeg\" class=\"slide-img\">\r\n  <div class=\"text\">Today in this blog I\'ll share the image slideshow effect using javascript. In this program, there are five images, and these images automatically change after a certain period. I used JavaScript to change the image after a certain time interval.\r\n  </div>\r\n</div>\r\n\r\n<div class=\"mySlides fade\">\r\n  <img src=\"img/place3.jpeg\" class=\"slide-img\">\r\n  <div class=\"text\">You can copy the codes from the given boxes or download the code files from the given link but I recommend you to download the source code files instead of copying codes because you won\'t get images if you copy the codes\r\n  </div>\r\n</div>');
+INSERT INTO `hall_pages` (`id`, `official_name`, `name`) VALUES
+(1, 'Sharpe Refectory', '\"The Ratty\"'),
+(2, 'Andrews Commons', '\"Andrews Commons\"'),
+(3, 'Verney-Woolley', '\"Verney-Woolley\"'),
+(4, 'Ivy Room', '\"Ivy Room\"'),
+(5, 'Josiah\'s', '\"Josiah\'s\"');
 
 -- --------------------------------------------------------
 
@@ -101,9 +141,29 @@ INSERT INTO `hall_slideshow` (`hall_id`, `img_src`, `assoc_text`) VALUES
 (2, 'img/place4.jpeg', 'some text(1)'),
 (2, 'img/placeholding.png', 'some text(2)');
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reviews`
+--
+
+CREATE TABLE `reviews` (
+  `id` int(11) NOT NULL,
+  `author_id` int(11) NOT NULL,
+  `dish_id` int(11) NOT NULL,
+  `rating` int(11) NOT NULL,
+  `content` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `accounts`
+--
+ALTER TABLE `accounts`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `dish_pages`
@@ -116,6 +176,14 @@ ALTER TABLE `dish_pages`
 --
 ALTER TABLE `hall_pages`
   ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `reviews`
+--
+ALTER TABLE `reviews`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `dish_id` (`dish_id`),
+  ADD KEY `author_id` (`author_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -132,6 +200,23 @@ ALTER TABLE `dish_pages`
 --
 ALTER TABLE `hall_pages`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `reviews`
+--
+ALTER TABLE `reviews`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `reviews`
+--
+ALTER TABLE `reviews`
+  ADD CONSTRAINT `reviews_ibfk_1` FOREIGN KEY (`dish_id`) REFERENCES `dish_pages` (`id`),
+  ADD CONSTRAINT `reviews_ibfk_2` FOREIGN KEY (`author_id`) REFERENCES `accounts` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
