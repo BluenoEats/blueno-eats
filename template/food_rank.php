2b8a9d23c5bc0973bd3cc489cbb31162197c8652
tlamlert <tanadol_lamlertprasertkul@brown.edu>
2022-03-13 14:26:03 -0400

    working ranking

--- template/food_rank.php
+++ template/food_rank.php
@@ -10,26 +10,29 @@ function get_info($dbc, $dish_id)
   $name = mysqli_fetch_assoc($result)['name'];
 
   // get first image from slideshow
-  $result = get_slideshow($dbc, DISH_SLIDESHOW_DB, 'dish_id', $dish_id);
+  $query = "SELECT * FROM " . DISH_SLIDESHOW_DB . " WHERE dish_id = $dish_id";
+  $result = mysqli_query($dbc, $query);
   $image = mysqli_num_rows($result) ? mysqli_fetch_assoc($result)['img_src'] : null;
 
   // return 1.name 2.image
   return [$name, $image];
 }
 
-function echo_food_list($dbc, $dish_list)
+function echo_food_list($dbc, $dish_list, $symbol)
 {
-  foreach ($dish_list as $dish) {
+  foreach ($dish_list as $rank=>$dish) {
     [$name, $image] = get_info($dbc, $dish['dish_id']);
     $rating = number_format($dish['rating'], 1, '.', '');
 ?>
 
     <div class="rank-card">
       <img src="<?php echo ($image != null) ? $image : 'img/place2.jpeg'; ?>" alt="img" style="width:100%">
-      <h2><i class="fa fa-crown"></i> <i class="fa fa-crown"></i> <i class="fa fa-crown"></i></h2>
+      <h2><?php for($i = 3; $i > $rank; $i--) {
+        echo '<i class="fa fa-' . $symbol . '"></i>';
+      } ?></h2>
       <p class="rank-title"><?php echo $name; ?></p>
       <p>Rating: <?php echo $rating; ?></p>
-      <p><button>Check the Reviews</button></p>
+      <p><button><a href="food.php?dish=<?php echo $dish['dish_id']; ?>">Check the Reviews</a></button></p>
     </div>
 
 <?php
@@ -43,16 +46,16 @@ function echo_food_list($dbc, $dish_list)
   $query = "SELECT dish_id, SUM(rating)/COUNT(rating) as rating FROM " . REVIEW_DB . " GROUP BY dish_id ORDER BY rating DESC LIMIT $NUM_RECOMMENDATION";
   $result = mysqli_query($dbc, $query);
   $dish_list = mysqli_fetch_all($result, MYSQLI_ASSOC);
-  echo_food_list($dbc, $dish_list);
+  echo_food_list($dbc, $dish_list, 'crown');
   ?>
 </div>
 
 <h1 class="rank-head">Worst-rated Food</h1>
 <div class="rank">
-<?php
-$query = "SELECT dish_id, SUM(rating)/COUNT(rating) as rating FROM " . REVIEW_DB . " GROUP BY dish_id ORDER BY rating ASC LIMIT $NUM_RECOMMENDATION";
-$result = mysqli_query($dbc, $query);
-$dish_list = mysqli_fetch_all($result, MYSQLI_ASSOC);
-echo_food_list($dbc, $dish_list);
-?>
+  <?php
+  $query = "SELECT dish_id, SUM(rating)/COUNT(rating) as rating FROM " . REVIEW_DB . " GROUP BY dish_id ORDER BY rating ASC LIMIT $NUM_RECOMMENDATION";
+  $result = mysqli_query($dbc, $query);
+  $dish_list = mysqli_fetch_all($result, MYSQLI_ASSOC);
+  echo_food_list($dbc, $dish_list, 'ghost');
+  ?>
 </div>
