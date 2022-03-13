862e7a471d7e213e1eac36ba237daa160527dcfc
tlamlert <tanadol_lamlertprasertkul@brown.edu>
2022-03-13 14:05:21 -0400

    working food rank

--- template/food_rank.php
+++ template/food_rank.php
@@ -1,97 +1,57 @@
 <?php
-$num_dishes = 5;
+$NUM_RECOMMENDATION = 3;
 
-function get_dish_name($dbc, $dish_id)
+function get_info($dbc, $dish_id)
 {
+  // get dish name from dish page
   $query = "SELECT name FROM " . DISH_DB . " WHERE id = $dish_id";
   $result = mysqli_query($dbc, $query);
-  $page = mysqli_fetch_assoc($result);
+  $name = mysqli_fetch_assoc($result)['name'];
 
-  return $page['name'];
+  // get first image from slideshow
+  $result = get_slideshow($dbc, DISH_SLIDESHOW_DB, 'dish_id', $dish_id);
+  $image = mysqli_num_rows($result) ? mysqli_fetch_assoc($result)['img_src'] : null;
+
+  // return 1.name 2.image
+  return [$name, $image];
 }
 
-function echo_food_list($dbc, $result)
+function echo_food_list($dbc, $dish_list)
 {
-  while ($row = mysqli_fetch_assoc($result)) {
-    $dish_id = $row['dish_id'];
-    $dish_name = get_dish_name($dbc, $dish_id);
-    $rating = number_format($row['rating'], 1, '.', '');
+  foreach ($dish_list as $dish) {
+    [$name, $image] = get_info($dbc, $dish['dish_id']);
+    $rating = number_format($dish['rating'], 1, '.', '');
 ?>
-    <li><a href="food.php?dish=<?php echo $dish_id; ?>">
-        <?php echo $dish_name ?> (rating: <?php echo $rating; ?>)
-      </a></li>
-<?php }
+
+    <div class="rank-card">
+      <img src="<?php echo ($image != null) ? $image : 'img/place2.jpeg'; ?>" alt="img" style="width:100%">
+      <h2><i class="fa fa-crown"></i> <i class="fa fa-crown"></i> <i class="fa fa-crown"></i></h2>
+      <p class="rank-title"><?php echo $name; ?></p>
+      <p>Rating: <?php echo $rating; ?></p>
+      <p><button>Check the Reviews</button></p>
+    </div>
+
+<?php
+  }
 }
 ?>
 
+<h2>Best-rated Food</h2>
 <div class="rank">
-  <div class="flex-item flex-left">
-    <h2>Best-rated Food</h2>
-    <ol>
-      <?php
-      $query = "SELECT dish_id, SUM(rating)/COUNT(rating) as rating FROM " . REVIEW_DB . " GROUP BY dish_id ORDER BY rating DESC LIMIT $num_dishes";
-      $result = mysqli_query($dbc, $query);
-      echo_food_list($dbc, $result);
-      ?>
-    </ol>
-  </div>
-
-  <div class="flex-item flex-right">
-    <h2>Worst-rated Food</h2>
-    <ol>
-      <?php
-      $query = "SELECT dish_id, SUM(rating)/COUNT(rating) as rating FROM " . REVIEW_DB . " GROUP BY dish_id ORDER BY rating ASC LIMIT $num_dishes";
-      $result = mysqli_query($dbc, $query);
-      echo_food_list($dbc, $result);
-      ?>
-    </ol>
-  </div>
+  <?php
+  $query = "SELECT dish_id, SUM(rating)/COUNT(rating) as rating FROM " . REVIEW_DB . " GROUP BY dish_id ORDER BY rating DESC LIMIT $NUM_RECOMMENDATION";
+  $result = mysqli_query($dbc, $query);
+  $dish_list = mysqli_fetch_all($result, MYSQLI_ASSOC);
+  echo_food_list($dbc, $dish_list);
+  ?>
 </div>
 
+<h2>Worst-rated Food</h2>
 <div class="rank">
-  <div class="rank-card">
-    <img src="img/place2.jpeg" alt="img" style="width:100%">
-    <h2><i class="fa fa-crown"></i> <i class="fa fa-crown"></i> <i class="fa fa-crown"></i></h2>
-    <p class="rank-title">Grilled Chicken</p>
-    <p>Rating: 5</p>
-    <p><button>Check the Reviews</button></p>
-  </div>
-  <div class="rank-card">
-    <img src="img/place2.jpeg" alt="img" style="width:100%">
-    <h2><i class="fa fa-crown"></i> <i class="fa fa-crown"></i></h2>
-    <p class="rank-title">Grilled Chicken</p>
-    <p>Rating: 5</p>
-    <p><button>Check the Reviews</button></p>
-  </div>
-  <div class="rank-card">
-    <img src="img/place2.jpeg" alt="img" style="width:100%">
-    <h2><i class="fa fa-crown"></i></h2>
-    <p class="rank-title">Grilled Chicken</p>
-    <p>Rating: 5</p>
-    <p><button>Check the Reviews</button></p>
-  </div>
-</div>
-
-<div class="rank">
-  <div class="rank-card">
-    <img src="img/place2.jpeg" alt="img" style="width:100%">
-    <h2><i class="fa-solid fa-ghost"></i> <i class="fa-solid fa-ghost"></i> <i class="fa fa-solid fa-ghost"></i></h2>
-    <p class="rank-title">Grilled Chicken</p>
-    <p>Rating: 5</p>
-    <p><button>Check the Reviews</button></p>
-  </div>
-  <div class="rank-card">
-    <img src="img/place2.jpeg" alt="img" style="width:100%">
-    <h2><i class="fa-solid fa-ghost"></i> <i class="fa-solid fa-ghost"></i></h2>
-    <p class="rank-title">Grilled Chicken</p>
-    <p>Rating: 5</p>
-    <p><button>Check the Reviews</button></p>
-  </div>
-  <div class="rank-card">
-    <img src="img/place2.jpeg" alt="img" style="width:100%">
-    <h2><i class="fa-solid fa-ghost"></i></h2>
-    <p class="rank-title">Grilled Chicken</p>
-    <p>Rating: 5</p>
-    <p><button>Check the Reviews</button></p>
-  </div>
+<?php
+$query = "SELECT dish_id, SUM(rating)/COUNT(rating) as rating FROM " . REVIEW_DB . " GROUP BY dish_id ORDER BY rating ASC LIMIT $NUM_RECOMMENDATION";
+$result = mysqli_query($dbc, $query);
+$dish_list = mysqli_fetch_all($result, MYSQLI_ASSOC);
+echo_food_list($dbc, $dish_list);
+?>
 </div>
